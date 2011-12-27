aedb65d2e621417a174604a99b0844e2a965e208
Sanjeev Kumar Mishra <sanjusoftware@gmail.com>
2011-12-27 13:47:08 +0530

    added the dashboard projects page

--- config/routes.rb
+++ config/routes.rb
@@ -81,7 +81,7 @@ Agiledme::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "projects#index"
+  root :to => "dashboard#index"
 
   # See how all your routes lay out with "rake routes"
 

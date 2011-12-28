585c36b08ccea479df6f1eab34f3526813de257e
Sanjeev Kumar Mishra <sanjusoftware@gmail.com>
2011-12-29 00:11:45 +0530

    got panels closing properly

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$("#backlog_close").click(function () {
+    $("#backlog").hide();
+});
+$("#current_close").click(function () {
+    $("#current").hide();
+});
+$("#done_close").click(function () {
+    $("#done").hide();
+});
+$("#icebox_close").click(function () {
+    $("#icebox").hide();
+});

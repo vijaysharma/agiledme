4c44ecf0783b8ab1cdab8d623a2a2b99ef8f9ed8
Sanjeev Kumar Mishra <sanjusoftware@gmail.com>
2012-01-01 23:15:56 +0530

    adding task is successful, just need to sert the description as well now

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,8 @@
 module ProjectsHelper
+  def add_task_link(name, form)
+    link_to_function name do |page|
+      task = render(:partial => 'workable_items/task', :locals => {:project_form => form, :task => Task.new})
+      page << %{ $('"#{ escape_javascript task }"').appendTo('#tasks'); }
+    end
+  end
 end

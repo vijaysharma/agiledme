49fbe782fd7630d98d8e2b394939fb10dab094ba
Sanjeev Kumar Mishra <sanjusoftware@gmail.com>
2012-01-02 18:00:02 +0530

    got the tasking finishing and deleting working

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -3,8 +3,6 @@ module ProjectsHelper
     link_to_function name do |page|
       task = render(:partial => 'workable_items/new_task', :locals => {:project_form => form, :task => Task.new})
       page << %{
-    alert($('#latest_task_description').val());
-    alert("#{ escape_javascript task }");
     $("#{ escape_javascript task }").appendTo('#tasks');
   }
     end

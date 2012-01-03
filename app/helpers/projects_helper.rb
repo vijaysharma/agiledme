edb8f43f32e6198cb8f8f93bc3fd067ab3f6d934
Sanjeev Kumar Mishra <sanjusoftware@gmail.com>
2012-01-03 23:20:14 +0530

    comments done

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,5 +1,19 @@
 module ProjectsHelper
 
+  def link_to_add_task_fields(name, f, association, item_id)
+    new_object = f.object.class.reflect_on_association(association).klass.new
+    fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
+      render("workable_items/"+association.to_s.singularize + "_fields", :f => builder)
+    end
+    link_to_function(name, ("add_task_fields(this, '#{association.to_s.singularize}', '#{escape_javascript(fields)}')"), :id => "#{item_id}_add_task")
+  end
 
+  def link_to_add_comment_fields(name, f, association, item_id)
+    new_object = f.object.class.reflect_on_association(association).klass.new
+    fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
+      render("workable_items/"+association.to_s.singularize + "_fields", :f => builder)
+    end
+    link_to_function(name, ("add_comment_fields(this, '#{association.to_s.singularize}', '#{escape_javascript(fields)}')"), :id => "#{item_id}_add_comment")
+  end
 
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :workable_item
+
+  before_create :add_posted_by
   after_create :add_workable_item_history
+
+  validates_presence_of :comment
 
   def owner
     User.find(self.posted_by)
@@ -10,6 +14,10 @@ class Comment < ActiveRecord::Base
 
   def add_workable_item_history
     self.workable_item.add_history("added comment : " + comment)
+  end
+
+  def add_posted_by
+    self.posted_by = User.current_user.id
   end
 
 

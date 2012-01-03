49606f2d2d2654635f57c302aabf0d1e48bcc60c
Sanjeev Kumar Mishra <sanjusoftware@gmail.com>
2012-01-03 13:31:54 +0530

    got the adding task to the top of list working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,12 +4,12 @@ module ApplicationHelper
     content_for(:title) { page_title }
   end
 
-  def link_to_add_fields(name, f, association)
+  def link_to_add_fields(name, f, association, item_id)
     new_object = f.object.class.reflect_on_association(association).klass.new
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
       render("workable_items/"+association.to_s.singularize + "_fields", :f => builder)
     end
-    link_to_function(name, ("add_fields(this, '#{association.to_s.singularize}', '#{escape_javascript(fields)}')"))
+    link_to_function(name, ("add_fields(this, '#{association.to_s.singularize}', '#{escape_javascript(fields)}')"), :id => "#{item_id}_add_task")
   end
 
 end

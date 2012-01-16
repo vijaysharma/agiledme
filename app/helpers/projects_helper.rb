7d8803d91f9afa7427088c06cadcaaa8c814b429
Sanjeev Kumar Mishra <sanjusoftware@gmail.com>
2012-01-16 16:16:54 +0530

    handled the nil case and replaced the velocity heading

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -17,22 +17,23 @@ module ProjectsHelper
   end
 
   def velocity_chart_series(project, start_time)
-
+    workable_items = nil
     if !project.estimate_bugs? and !project.estimate_chores?
-     workable_items = project.workable_items.where(" status = 'delivered'")
+      workable_items = project.workable_items.where(" status = 'delivered'")
     elsif project.estimate_bugs?
       workable_items = project.workable_items.where(" status = 'delivered' and type != 'Chore'")
     elsif project.estimate_chores?
       workable_items = project.workable_items.where(" status = 'delivered' and type != 'Bug'")
     end
-
-    workable_items_by_day = workable_items.where(:delivered_at => start_time.beginning_of_day..Time.zone.now.end_of_day).
-                    group("date(delivered_at)").
-                    select("delivered_at, sum(estimate) as estimate")
-    (start_time.to_date..Date.today).map do |date|
-      workable_item = workable_items_by_day.detect { |workable_item| workable_item.delivered_at.to_date == date }
-      workable_item && workable_item.estimate || 0
-    end.inspect
+    if workable_items
+      workable_items_by_day = workable_items.where(:delivered_at => start_time.beginning_of_day..Time.zone.now.end_of_day).
+          group("date(delivered_at)").
+          select("delivered_at, sum(estimate) as estimate")
+      (start_time.to_date..Date.today).map do |date|
+        workable_item = workable_items_by_day.detect { |workable_item| workable_item.delivered_at.to_date == date }
+        workable_item && workable_item.estimate || 0
+      end.inspect
+    end
   end
 
 end

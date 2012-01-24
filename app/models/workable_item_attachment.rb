5620a07fadd461321b4d9c68d952c58a63eacd17
Sanjeev Kumar Mishra <sanjusoftware@gmail.com>
2012-01-24 20:39:34 +0530

    got the file upload working fine second cut

--- app/models/workable_item_attachment.rb
+++ app/models/workable_item_attachment.rb
@@ -1,6 +1,6 @@
 class WorkableItemAttachment < ActiveRecord::Base
   belongs_to :workable_item
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :image, :styles => {:thumb => "72x52>" }
 
 end

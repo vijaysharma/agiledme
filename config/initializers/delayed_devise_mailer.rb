4ef457c369e79dde00d4ac107c8b7157f942f62f
Sanjeev Kumar Mishra <sanjusoftware@gmail.com>
2012-02-14 17:34:42 +0530

    disabled delayed job for heruko

--- config/initializers/delayed_devise_mailer.rb
+++ config/initializers/delayed_devise_mailer.rb
@@ -1,20 +1,20 @@
-module Devise
-  module Models
-    module Confirmable
-      handle_asynchronously :send_confirmation_instructions
-    end
-
-    module Recoverable
-      handle_asynchronously :send_reset_password_instructions
-    end
-
-    module Lockable
-      handle_asynchronously :send_unlock_instructions
-    end
-
-    module Invitable
-      #had to make the deliver_invitation public instead of protected for this to work..hack!!
-      handle_asynchronously :deliver_invitation
-    end
-  end
-end
+#module Devise
+#  module Models
+#    module Confirmable
+#      handle_asynchronously :send_confirmation_instructions
+#    end
+#
+#    module Recoverable
+#      handle_asynchronously :send_reset_password_instructions
+#    end
+#
+#    module Lockable
+#      handle_asynchronously :send_unlock_instructions
+#    end
+#
+#    module Invitable
+#      #had to make the deliver_invitation public instead of protected for this to work..hack!!
+#      handle_asynchronously :deliver_invitation
+#    end
+#  end
+#end

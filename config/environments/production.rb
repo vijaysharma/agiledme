e773c01000963a1b3ef970a9b795f93ba230a25c
Sanjeev Kumar Mishra <sanjusoftware@gmail.com>
2012-02-15 10:35:06 +0530

    fixed the host parameter for the mails purpose

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,6 +43,7 @@ Agiledme::Application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
+  config.action_mailer.default_url_options = {:host => 'castletrack.herokuapp.com'}
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify

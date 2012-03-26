90cc82cacd94f37a7e282158632e1a415f12e35f
Sanjeev Kumar Mishra <sanjusoftware@gmail.com>
2012-03-27 00:43:27 +0530

    upgraded to the rails 3.2.2 to leverage asset pipeline

--- Gemfile
+++ Gemfile
@@ -1,9 +1,17 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.0.3'
+gem 'rails', '3.2.2'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails', "~> 3.2.3"
+  gem 'coffee-rails', "~> 3.2.1"
+  gem 'uglifier', '>= 1.0.3'
+end
 
 gem 'pg'
 gem 'devise'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Agiledme::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
   # Specifies the header that your server uses for sending files
@@ -47,4 +47,15 @@ Agiledme::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
+
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false
+
+  # Generate digests for assets URLs
+  config.assets.digest = true
+
+
 end
